ac496724ea116d1cc893602d6f4966942013e64a
Ricardo Gonçalves <yourmother_bel@hotmail.com>
2026-02-23 13:36:59 +0000

    add skeleton definition of the keyboard class

--- input/keyboard.py
+++ input/keyboard.py
@@ -1,1 +1,39 @@
-pass
+class Piano:
+    """ 
+    A Simple 'Piano' that, in turn, represents 
+    the keyboard of the machine that's using
+    this Project. 
+    """
+
+    def __init__(self):
+        """ 
+        Default Constructor
+        
+        :param self: Piano Object
+        """
+        pass
+
+    def start_listening(self):
+        """ 
+        Method to Start Listening. 
+
+        :param self: Piano Object
+        """
+        pass
+
+    def stop_listening(self):
+        """ 
+        Method to Stop Listening. 
+
+        :param self: Piano Object
+        """
+        pass
+
+    def on_press(self, key) -> None:
+        """
+        On Press Keyboard Method. 
+
+        :param self: Piano Object
+        :param key: Pressed Key
+        """
+        pass
