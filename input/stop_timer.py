ef2c5e604fd01bf204a74f11c030d4bc846e109b
Ricardo Gonçalves <yourmother_bel@hotmail.com>
2026-02-23 13:40:21 +0000

    add skeleton definition of the stop timer class

--- input/stop_timer.py
+++ input/stop_timer.py
@@ -1,1 +1,30 @@
-pass
+class StopTimer:
+    """
+    Stop Timer to Control Any
+    Kind of Time Operations that would
+    be needed by this Project.
+    """ 
+
+    def __init__(self):
+        """ 
+        Parametrized Constructor 
+
+        :param self: StopTimer Object
+        """
+        pass
+
+    def on_timeout(self) -> None: 
+        """ 
+        On Timeout Method of the Timer. 
+        
+        :param self: StopTimer Object
+        """
+        pass
+
+    def start_stop_timer(self) -> None: 
+        """ 
+        Start Method for the Timer. 
+
+        :param self: StopTimer Object
+        """
+        pass
